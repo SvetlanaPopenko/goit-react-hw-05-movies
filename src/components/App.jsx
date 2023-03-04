fe9fa3012e70caeaec663dfe181743f59d552677
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-04 03:34:52 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
-import MovieDetails from 'pages/MovieDetails';
-import Movies from 'pages/Movies';
-import Home from 'pages/Home';
+import { lazy } from "react";
 import { Routes, Route } from 'react-router-dom';
-import NotFound from './NotFound/NotFound';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
 import { SharedLayout } from './SharedLayout/SharedLayout';
+
+const Home = lazy(() => import("../pages/Home"));
+const Movies = lazy(() => import("../pages/Movies"));
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+const Cast = lazy(() => import("./Cast/Cast"));
+const Reviews = lazy(() => import("./Reviews/Reviews"));
+const NotFound = lazy(() => import("./NotFound/NotFound"));
+
 
 export const App = () => {
   return (
