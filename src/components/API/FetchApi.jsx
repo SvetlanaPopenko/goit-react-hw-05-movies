6effd6fe21f02f3f24235551c9fbeedbd126ef4c
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-04 03:34:49 +0200

    Create FetchApi.jsx

---
+++ src/components/API/FetchApi.jsx
@@ -0,0 +1,64 @@
+import axios, { spread } from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'f983fc840eb543faba07dcbe6db19b0b';
+
+export const getTrendingMovie = async (pageNumber = 1, options) => {
+  try {
+    const response = await axios.get(
+      `trending/all/day?api_key=${API_KEY}&page=${pageNumber}`,
+      options
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchSearchMovie = async (searchQuery, options) => {
+  try {
+    const response = await axios.get(
+      `search/movie?api_key=${API_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`,
+      options
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMovieDetails = async (movieId, options) => {
+  try {
+    const response = await axios.get(
+      `movie/${movieId}?api_key=${API_KEY}&language=en-US`,
+      options
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMovieCredits = async (movieId, options) => {
+  try {
+    const response = await axios.get(
+      `movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`,
+      options
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMovieReviews = async (movieId, options) => {
+  try {
+    const response = await axios.get(
+      `movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`,
+      options
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
