7d4486e77b30d067fe4c2738bf7f4e652faef67a
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-04 03:35:01 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,24 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 const Movies = () => {
-    return <div></div>
+    useEffect(() => {
+        const controller = new AbortController();
+
+        async function fetchData(page) {
+            try {
+                const response = await axios.get(page,{signal: controller.signal});
+                console.log(response.data);
+            } catch (error) {
+                
+            }
+        }
+        fetchData();
+        return () => {
+            controller.abort();
+        }
+    }, []);
+    return <div>other page</div>
 };
 
 export default Movies;
