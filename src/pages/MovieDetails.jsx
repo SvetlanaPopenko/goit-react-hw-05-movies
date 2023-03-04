76ba8c05225dcc2f1feb103c0675683f827842cd
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-04 03:34:56 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,19 +1,27 @@
-import { useParams } from "react-router-dom";
-import { Link, Outlet } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import { BackLink } from 'components/BackLink';
+import { useRef } from 'react';
 
 const MovieDetails = () => {
-    const {movieId} = useParams();
-    return (<div>{movieId}
-        <ul>
-            <li>
-                <Link to="cast">Cast</Link>
-            </li>
-            <li>
-                <link to="reviews">Reviews</link>
-            </li>
-        </ul>
-        <Outlet/>
-    </div>)
+//   const { movieId } = useParams();
+  const { location } = useLocation();
+  const ref = useRef(location.state?.from ?? '/movies');
+
+  return (
+    <div>
+      <BackLink to={ref.current}>Back to movies</BackLink>
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <link to="reviews">Reviews</link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
 };
 
 export default MovieDetails;
