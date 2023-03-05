e9c444dc87a033b895174405bd3630bc3e0dacf2
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-06 01:51:05 +0200

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,23 @@
+import { Loader } from 'components/Loader/Loader';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { StyledLink, Container } from './SharedLayout.styled';
+import { StyledLink, Container, Header } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
     <Container>
-      <nav>
-        <StyledLink to="/" end>
-          Home
-        </StyledLink>
-        <StyledLink to="/movies">Movies</StyledLink>
-          </nav>
-          <Outlet/>
+      <Header>
+        <nav>
+          <StyledLink to="/" end>
+            Home
+          </StyledLink>
+          <StyledLink to="/movies">Movies</StyledLink>
+        </nav>
+      </Header>
+
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
     </Container>
   );
 };
