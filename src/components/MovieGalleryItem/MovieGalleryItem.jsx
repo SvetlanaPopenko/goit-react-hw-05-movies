9e556b0b583f414f46c055a9e83484cffb6d770c
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-05 03:15:58 +0200

    Create MovieGalleryItem.jsx

---
+++ src/components/MovieGalleryItem/MovieGalleryItem.jsx
@@ -0,0 +1,41 @@
+import { useLocation } from 'react-router-dom';
+import { StyledLink } from './MovieGalleryItem.styled';
+import PropTypes from 'prop-types';
+
+export const MovieGalleryItem = ({
+  movie: { title, name, poster_path, release_date, id },
+}) => {
+  const location = useLocation();
+  return (
+    <div>
+      <StyledLink to={`movies/${id}`} state={{ from: location }}>
+        <div>
+          <div>
+            <img
+              src={
+                poster_path
+                  ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                  : ''
+              }
+              alt={title || name}
+            />
+          </div>
+          <div>
+            <h2>{title || name}</h2>
+                      <p>Release data : { release_date?.slice(0, 4) || 'not found'}</p>
+          </div>
+        </div>
+      </StyledLink>
+    </div>
+  );
+};
+
+MovieGalleryItem.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    name: PropTypes.string,
+    poster_path: PropTypes.string,
+    release_date: PropTypes.string,
+    id: PropTypes.number,
+  }),
+};
