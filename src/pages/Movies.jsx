8f9d9d982255695ca0f6f48dc8dbae3070563fc2
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-06 01:51:07 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,24 +1,81 @@
-import axios from 'axios';
-import { useEffect } from 'react';
+import { fetchSearchMovie } from 'components/API/FetchApi';
+import { Button } from 'components/Button/Button';
+import { Loader } from 'components/Loader/Loader';
+import { MoviesGallery } from 'components/MoviesGallery/MoviesGallery';
+import { SearchBar } from 'components/SearchBar/SearchBar';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
-    useEffect(() => {
-        const controller = new AbortController();
+  const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const movieName = searchParams.get('query') ?? '';
 
-        async function fetchData(page) {
-            try {
-                const response = await axios.get(page,{signal: controller.signal});
-                console.log(response.data);
-            } catch (error) {
-                
-            }
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    const getMovieBySearch = async (query, page) => {
+      try {
+        const data = await fetchSearchMovie(query, page, {
+          signal: controller.signal,
+        });
+
+        if (!data.results) {
+          return;
         }
-        fetchData();
-        return () => {
-            controller.abort();
-        }
-    }, []);
-    return <div>other page</div>
+        setTotalPages(data.total_pages);
+        setMovies(prevMovies => {
+          return page === 1 ? data.results : [...prevMovies, ...data.results];
+        });
+        return data;
+      } catch (error) {
+        setMovies([]);
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (!searchQuery) {
+      return;
+    }
+    getMovieBySearch(searchQuery, page);
+    return () => {
+      controller.abort();
+    };
+  }, [searchQuery, page]);
+
+  const handleOnSearch = query => {
+    if (query === '' || query === searchQuery) {
+      return;
+    }
+    const nextParams = query !== '' ? { query } : {};
+    setSearchParams(nextParams);
+    setMovies([]);
+    setPage(1);
+    setSearchQuery(query);
+  };
+
+  const onLoad = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+    if (movies) {
+        return (
+            <main>
+                <SearchBar movieName={movieName} onSubmit={handleOnSearch} />
+                <div>
+                    <MoviesGallery movies={movies} />
+                </div>
+                {isLoading && <Loader />}
+                {!!movies.length && page < totalPages && (
+                    <Button onClick={onLoad}/>
+                                        )}
+            </main>
+        );
+    }
 };
 
 export default Movies;
