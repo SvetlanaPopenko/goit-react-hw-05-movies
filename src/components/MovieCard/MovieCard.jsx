ab9b3f57737d0dc3d025204f019a964ae5fb63c3
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-05 03:15:44 +0200

    Create MovieCard.jsx

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+
+export const MovieCard = ({
+  card: {
+    title,
+    name,
+    poster_path,
+    overview,
+    genres,
+    vote_average,
+    release_date,
+  },
+}) => {
+  return (
+    <div>
+      <div>
+        <img
+          src={
+            poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : ''
+          }
+          alt={title || name}
+        />
+      </div>
+      <div>
+        <h2>{title || name}</h2>
+        <p>Release data : { release_date?.slice(0, 4) || 'not found'}</p>
+        <p>
+          User score:<span>{(vote_average * 10).toFixed(1)}%</span>
+        </p>
+        <p>
+          Overview:<span>{overview}</span>
+        </p>
+        <p>
+          Genres:
+          <span>
+            {genres
+              .map(genre => {
+                return genre.name;
+              })
+              .join(', ')}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  card: PropTypes.shape({
+    title: PropTypes.string,
+    name: PropTypes.string,
+    poster_path: PropTypes.string,
+    overview: PropTypes.string,
+    genres: PropTypes.array,
+    vote_average: PropTypes.number,
+    release_date: PropTypes.string,
+  }),
+};
