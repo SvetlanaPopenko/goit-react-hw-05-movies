a95127e3faea74496a2509753c7a1a3e6ac89fd8
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-06 01:51:13 +0200

    Create SearchBar.jsx

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,35 @@
+import {
+  SearchbarHeader,
+  SearchForm,
+  SearchFormButton,
+  SearchFormLabel,
+  SearchFormInput,
+} from './SearchBar.styled';
+import { BsSearch } from 'react-icons/bs';
+
+export const SearchBar = ({ onSubmit }) => {
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    onSubmit(evt.target.search.value.trim().toLowerCase());
+    evt.currentTarget.reset();
+  };
+  return (
+    <SearchbarHeader>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormButton type="submit">
+          <BsSearch width={60} height={60} />
+          <SearchFormLabel>Search</SearchFormLabel>
+        </SearchFormButton>
+
+        <SearchFormInput
+          name="search"
+          type="text"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search movies"
+        />
+      </SearchForm>
+    </SearchbarHeader>
+  );
+};
+
