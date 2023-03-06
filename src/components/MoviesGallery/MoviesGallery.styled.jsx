eec16cac5bd0cf989ef04216932228afda6f92cf
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-07 01:57:31 +0200

    Update MoviesGallery.styled.jsx

--- src/components/MoviesGallery/MoviesGallery.styled.jsx
+++ src/components/MoviesGallery/MoviesGallery.styled.jsx
@@ -1,2 +1,11 @@
 import styled from "styled-components";
 
+export const MovieGalleryList=styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    margin: 0 auto;
+    flex-wrap:wrap;
+`
+
