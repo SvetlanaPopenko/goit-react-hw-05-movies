3bd09ae0568fe8fce177c51c7e6f6894b0c7e51d
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-07 01:57:11 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,5 +1,45 @@
+import { getMovieCredits } from "components/API/FetchApi";
+import { CastItem } from "components/CastItem/CastItem";
+import { Loader } from "components/Loader/Loader";
+import { Title } from "components/Title/Title";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Cast = () => {
-    return <div>cast</div>
+    const [isLoading, setIsLoading] = useState(false);
+    const [movieCasts, setMovieCasts] = useState([]);
+    const { movieId } = useParams();
+
+    useEffect(() => {
+        const controller = new AbortController();
+        setIsLoading(true);
+        const fetchMovieCast = async movieId => {
+      try {
+        const data = await getMovieCredits(movieId, {
+          signal: controller.signal,
+        });
+          setMovieCasts(data.cast);
+            return data;
+              } catch (error) {
+                console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchMovieCast(movieId);
+    return () => {
+      controller.abort();
+    };
+  }, [movieId]);
+
+    
+    return (<main>
+        <Title>Cast</Title>
+        <ul>
+            {movieCasts.map(movieCast => (<CastItem key={movieCast.id} movieCast={movieCast} />))}
+            {isLoading && <Loader/>}
+        </ul>
+    </main>)
 };
 
 export default Cast;
