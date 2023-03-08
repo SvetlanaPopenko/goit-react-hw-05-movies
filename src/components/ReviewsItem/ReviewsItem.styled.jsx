9489d1b629de19a1f991b9a52455f09b1e204503
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-08 04:01:36 +0200

    Update ReviewsItem.styled.jsx

---
+++ src/components/ReviewsItem/ReviewsItem.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ReviewItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 20px;
+  max-width: 960px;
+  border-radius: 10px;
+  border: 1px solid blue;
+  padding: 20px;
+`;
+export const ReviewTitle = styled.h3`
+  font-size: 26px;
+`;
+export const ReviewContent = styled.p`
+  font-size: 20px;
+`;
