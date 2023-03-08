2b9eb98cd7a5d7a7472b462df9091fb1eec2064b
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-08 04:01:28 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,27 +1,27 @@
-import { getMovieReviews } from "components/API/FetchApi";
-import { Loader } from "components/Loader/Loader";
-import { ReviewsItem } from "components/ReviewsItem/ReviewsItem";
-import { Title } from "components/Title/Title";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { getMovieReviews } from 'components/API/FetchApi';
+import { Loader } from 'components/Loader/Loader';
+import { ReviewsItem } from 'components/ReviewsItem/ReviewsItem';
+import { Section } from 'components/Section/Section';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Reviews = () => {
-    const [reviews, setReviews] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { movieId } = useParams();
 
-    useEffect(() => {
-         const controller = new AbortController();
-        setIsLoading(true);
-        const fetchMovieReview = async movieId => {
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    const fetchMovieReview = async movieId => {
       try {
         const data = await getMovieReviews(movieId, {
           signal: controller.signal,
         });
-          setReviews(data.results);
-            return data;
-              } catch (error) {
-                console.log(error);
+        setReviews(data.results);
+        return data;
+      } catch (error) {
+        console.log(error);
       } finally {
         setIsLoading(false);
       }
@@ -31,20 +31,18 @@ const Reviews = () => {
       controller.abort();
     };
   }, [movieId]);
-    
 
-    return (
-        <main>
-            <Title>Reviews</Title>
-            <ul>
-                {reviews.map(review => (
-                    <ReviewsItem key={review.id} review={review}/>
-                ))}
-                {isLoading && <Loader />}
-                {!!reviews.length||<p>We don`t have reviews for this movie</p>}
-            </ul>
-        </main>
-            )
+  return (
+    <Section title="Reviews">
+      <ul>
+        {reviews.map(review => (
+          <ReviewsItem key={review.id} review={review} />
+        ))}
+      </ul>
+      {isLoading && <Loader />}
+      {!!reviews.length || <p>We don`t have reviews for this movie</p>}
+    </Section>
+  );
 };
 
 export default Reviews;
