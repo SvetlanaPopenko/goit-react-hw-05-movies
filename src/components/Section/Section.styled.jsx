07cc33103b22dc9eb8c96ff6c59c1806d1fe637b
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-08 04:01:44 +0200

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SectionMain=styled.section`
+  padding: 30px 0;
+`
+export const Title = styled.h2`
+  text-align: center;
+  font-size: 32px;
+`;
