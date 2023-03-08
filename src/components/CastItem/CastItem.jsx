fe6e55bd77cd3a2642643174ed5a8f9c299a98e3
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-08 04:01:08 +0200

    Update CastItem.jsx

--- src/components/CastItem/CastItem.jsx
+++ src/components/CastItem/CastItem.jsx
@@ -1,16 +1,28 @@
-export const CastItem = ({ cast: { name, character, profile_path } }) => {
+import PropTypes from 'prop-types';
+import placeholder from '../../images/placeholder.png'
+import { CastImage } from './CastItem.styled';
+
+export const CastItem = ({ cast: { name, character, profile_path }, }) => {
   return (
     <li>
-      <img
+      <CastImage
         src={
-          profile_path ? `https://image.tmdb.org/t/p/w500${profile_path}` : ''
+          profile_path ? `https://image.tmdb.org/t/p/w500${profile_path}` : placeholder
         }
         alt={name}
-          />
-          <div>
-              <h2>{name}</h2>
-              <p>Character: {character||'No name' }</p>
-          </div>
+      />
+      <div>
+        <h2>{name}</h2>
+        <p>Character: {character || 'No name'}</p>
+      </div>
     </li>
   );
+};
+
+CastItem.propTypes = {
+  cast: PropTypes.shape({
+    name: PropTypes.string,
+    character: PropTypes.string,
+    profile_path: PropTypes.string,
+  }),
 };
