8046b529e81f7c4028b290c164eb88f6114f17f1
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-08 04:01:04 +0200

    Update Cast.styled.jsx

---
+++ src/components/Cast/Cast.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const CastList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
